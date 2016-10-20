8790f3ca87b7a163b14e91158a6f3d7838dc2cfa
Franklin <frank223c@gmail.com>
2016-10-20 14:00:06 -0500

    Agregando modelos en Asociacion

--- americas_service/americas_service_apps/asociacion/Lote.py
+++ americas_service/americas_service_apps/asociacion/Lote.py
@@ -2,22 +2,31 @@
 from uuid import uuid4
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+# models
+from .Manzana import Manzana
+from .ArregloManzana import ArregloManzana
 
 
 class Lote(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
 
-    codigo_lote = models.CharField(
-        _('arreglo lote'), unique=True, max_length=2, null=False)
+    manzana = models.ForeignKey(Manzana)
+    arreglo_manzana = models.ForeignKey(ArregloManzana)
+    # codigo_lote = models.CharField(
+    #     _('arreglo lote'), unique=True, max_length=2, null=False)
     area_total = models.DecimalField(
-        _('area total'), null=False, blank=False, decimal_places=2, max_digits=5)
+        _('area total'), null=False, blank=False,
+        decimal_places=2, max_digits=5)
+
     area_construida = models.DecimalField(
-        _('area construida'), null=False, blank=False, decimal_places=2, max_digits=5)
+        _('area construida'), null=False, blank=False,
+        decimal_places=2, max_digits=5)
     area_sin_construir = models.DecimalField(
-        _('area sin construir'), null=False, blank=False, decimal_places=2, max_digits=5)
+        _('area sin construir'), null=False, blank=False,
+        decimal_places=2, max_digits=5)
     # imagen = models.ImageField()
-    observaciones = models.TextField(max_length=500)
+    observaciones = models.TextField(max_length=500, null=True, blank=True)
     estado = models.BooleanField(default=True)
 
     class Meta:
@@ -25,4 +34,4 @@ class Lote(models.Model):
         verbose_name_plural = "Lotes"
 
     def __str__(self):
-        return '%s %s' % (self.id, self.codigo_lote, self.area_total)
+        return '%s - %s' % (self.manzana, self.arreglo_manzana)
--- americas_service/americas_service_apps/asociacion/Manzana.py
+++ americas_service/americas_service_apps/asociacion/Manzana.py
@@ -3,10 +3,8 @@ from uuid import uuid4
 # from datetime import datetime, timedelta
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-
-
-def validar_numero_maximo_lotes():
-    pass
+# models
+from .ArregloManzana import ArregloManzana
 
 
 class Manzana(models.Model):
@@ -17,12 +15,13 @@ class Manzana(models.Model):
 
     manzana = models.CharField(
         _('manzana'), unique=True, max_length=3, null=False, blank=False)
-    numero_lotes = models.PositiveSmallIntegerField(
-        _('numero lotes'), default=1, blank=False, null=False)
+    # numero_lotes = models.PositiveSmallIntegerField(
+    #     _('numero lotes'), default=1, blank=False, null=False)
+    arreglo_manzana = models.ForeignKey(ArregloManzana)
 
     class Meta:
         verbose_name = "Manzana"
         verbose_name_plural = "Manzanas"
 
     def __str__(self):
-        return '%s %s' % (self.manzana, self.numero_lotes)
+        return '%s' % (self.manzana)
