048498cd0419e2f76222bb2801c924b106d62ca9
Franklin <frank223c@gmail.com>
2016-10-26 07:46:01 -0500

    Mejorando relacion de tablas en RelacionManzana.py

--- americas_service/americas_service_apps/asociacion/models/RelacionManzana.py
+++ americas_service/americas_service_apps/asociacion/models/RelacionManzana.py
@@ -5,12 +5,14 @@ from .Lote import Lote
 
 class RelacionManzana(models.Model):
 
-    manzana = models.OneToOneField(Manzana)
-    lote = models.ManyToManyField(Lote)
+    # manzana = models.OneToOneField(Manzana)
+    manzana = models.ForeignKey(Manzana)
+    # lote = models.ManyToManyField(Lote)
+    lote = models.OneToOneField(Lote)
 
     class Meta:
         verbose_name = "RelacionManzana"
         verbose_name_plural = "RelacionManzanas"
 
     def __str__(self):
-        return 'Mz-%s' % (self.manzana)
+        return 'Mz-%s Lote - %s' % (self.manzana, self.lote)
--- americas_service/americas_service_apps/socio/models/SocioLote.py
+++ americas_service/americas_service_apps/socio/models/SocioLote.py
@@ -1,20 +1,21 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from americas_service_apps.socio.models.Socio import Socio
-from americas_service_apps.asociacion.models.Lote import Lote
+from americas_service_apps.asociacion.models.RelacionManzana import RelacionManzana
 
 
 class SocioLote(models.Model):
 
     socio = models.ForeignKey(Socio)
-    socio_lote = models.OneToOneField(Lote)
+    socio_lote = models.OneToOneField(RelacionManzana)
 
     class Meta:
         verbose_name = "SocioLote"
         verbose_name_plural = "SocioLotes"
 
     def __str__(self):
-        return '%s %s L-%s' % (
+        return '%s %s Mz-%s L-%s' % (
             self.socio.persona.first_name,
             self.socio.persona.documento,
-            self.socio_lote.input_lote)
+            self.socio_lote.manzana,
+            self.socio_lote.lote)
