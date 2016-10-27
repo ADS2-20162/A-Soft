0508a95daa327d192ca0d052af6e2752b9902c6f
erickdarwin <erickdarwin12396@gmail.com>
2016-10-27 11:02:22 -0500

    Ayuda de dante

--- americas_service/americas_service_apps/cobranza/admin.py
+++ americas_service/americas_service_apps/cobranza/admin.py
@@ -6,8 +6,10 @@ from .models.RubroCobranza import RubroCobranza
 from .models.ConceptoCobranza import ConceptoCobranza
 from .models.Mora import Mora
 from .models.Debe import Debe
+from .models.MontoCuota import MontoCuota
 
 admin.site.register(RubroCobranza)
 admin.site.register(ConceptoCobranza)
 admin.site.register(Debe)
 admin.site.register(Mora)
+admin.site.register(MontoCuota)
--- americas_service/americas_service_apps/cobranza/models/MontoCuota.py
+++ americas_service/americas_service_apps/cobranza/models/MontoCuota.py
@@ -1,11 +1,19 @@
 from django.db import models
+from .RubroCobranza import RubroCobranza
 
 
 class MontoCuota(models.Model):
+
+    rub_cobr = models.ForeignKey(RubroCobranza)
+    valor = models.DecimalField(decimal_places=2, max_digits=5, default=0.0)
+
+    def save(self, *args, **kwargs):
+        self.valor = self.rub_cobr.importe
+        return super(MontoCuota, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name = "MontoCuota"
         verbose_name_plural = "MontoCuotas"
 
     def __str__(self):
-        pass
+    	return('{0}'.format(self.valor))
